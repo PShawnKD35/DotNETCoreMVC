a07b6fec21a4c6bf5208218f5c564e3a6acbf175
Shawn Peng <lankeypeng@163.com>
2019-05-28 17:57:11 +0800

    Add project files.

---
+++ DotNetCoreMVC/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMVC.Controllers
+{
+    public class TodoController: Controller
+    {
+        public IActionResult Index()
+        {
+            // 从数据库获取 to-do 条目
+
+            // 把条目置于 model 中
+
+            // 使用 model 渲染视图
+        }
+    }
+}
