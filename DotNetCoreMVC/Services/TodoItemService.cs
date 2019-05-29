51a6d2ba5e63b8e4423dda1169cebb832ccbdbb7
Shawn Peng <lankeypeng@163.com>
2019-05-29 15:36:59 +0800

    [Add] Added database: mssql...

---
+++ DotNetCoreMVC/Services/TodoItemService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreMVC.Data;
+using DotNetCoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreMVC.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            return await _context.Items
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
+        }
+    }
+}
